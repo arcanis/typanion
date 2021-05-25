9e2f8bd240c789064dce1dbff7dac862834f256d
Christian Flach <cmfcmf.flach@gmail.com>
2021-05-25 16:15:01 +0200

    Allow readonly arrays in isEnum and isOneOf, fixes #7 (#10)
    * Allow readonly arrays in isEnum and isOneOf, fixes #7
    
    * Stylistic changes
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- tests/type-inferences.ts
+++ tests/type-inferences.ts
@@ -32,3 +32,44 @@ import * as t from '../sources';
 
     const quuz: MyType = {foo: undefined};
 }
+
+{
+    const schema = t.isEnum([`a`, `b`]);
+    type MyType = t.InferType<typeof schema>;
+
+    // @ts-expect-error
+    const foo: MyType = `c`;
+}
+
+{
+    const schema = t.isEnum([`a`, `b`] as const);
+    type MyType = t.InferType<typeof schema>;
+
+    // @ts-expect-error
+    const foo: MyType = `c`;
+}
+
+{
+    const schema = t.isObject({
+        enum: t.isEnum([`a`, `b`] as const)
+    });
+    type MyType = t.InferType<typeof schema>;
+
+    // @ts-expect-error
+    const foo: MyType = {enum: `c`};
+}
+
+// Does not currently work, see #9
+// {
+//     const schema = t.isObject({
+//         enum: t.isEnum([`a`, `b`])
+//     } as const);
+//     type MyType = t.InferType<typeof schema>;
+//
+//     // @ts-expect-error
+//     const foo: MyType = {enum: `c`};
+// }
+
+{
+    t.isOneOf([] as const);
+}
